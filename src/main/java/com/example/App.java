e32af99fc0199d1eb065a8c21d24643179932957
EyaGhrd <eya.ghared@gmail.com>
2024-12-17 17:42:41 +0100

    jen fixed

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,12 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello for Jenkins!");
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
