94f12a03368744957654e7298788d35c84745845
EyaGhrd <eya.ghared@gmail.com>
2024-12-20 22:38:27 +0100

    last

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,12 +3,5 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello for Jenkins!");
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
